a275fa57eda55d3bbd23a1604e89d078d5139f30
thurain <thurainbowin.dev@gmail.com>
2022-10-24 18:22:29 +0630

    - add profile section viewpod

--- app/src/main/java/com/thurainx/taskapp/viewholders/ProfileViewHolder.kt
+++ app/src/main/java/com/thurainx/taskapp/viewholders/ProfileViewHolder.kt
@@ -6,21 +6,22 @@ import com.thurainx.taskapp.data.vos.ProfileVO
 import com.thurainx.taskapp.delegates.ProfileDelegate
 import kotlinx.android.synthetic.main.view_holder_profile.view.*
 
-class ProfileViewHolder(itemView: View, delegate: ProfileDelegate) : RecyclerView.ViewHolder(itemView) {
+class ProfileViewHolder(itemView: View, delegate: ProfileDelegate) :
+    RecyclerView.ViewHolder(itemView) {
 
     var mProfile: ProfileVO? = null
 
     init {
 
-            itemView.setOnClickListener {
-                mProfile?.let {
-                    delegate.onTapProfile(it)
-                }
+        itemView.setOnClickListener {
+            mProfile?.let {
+                delegate.onTapProfile(it)
             }
+        }
 
     }
 
-    fun bind(profileVO: ProfileVO){
+    fun bind(profileVO: ProfileVO) {
         mProfile = profileVO
         itemView.ivProfileImage.setImageResource(profileVO.profileImage)
     }
---
+++ app/src/main/java/com/thurainx/taskapp/views/viewpods/ProfileSectionViewPod.kt
@@ -0,0 +1,14 @@
+package com.thurainx.taskapp.views.viewpods
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class ProfileSectionViewPod @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : ConstraintLayout(context, attrs) {
+
+
+
+}
